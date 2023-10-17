c35d6f16c763f001ac5d70bded2627aff3342dee
Marcelinus Alvinanda <marcelalvin11@gmail.com>
2023-10-17 21:19:54 +0700

    perbaikan

--- file_server.py
+++ file_server.py
@@ -13,7 +13,7 @@ def send_file(client_socket, filename):
     try:
         with open(filename, 'rb') as file:
             file_size = os.path.getsize(filename)  # Get the file size
-            client_socket.send(file_size)
+            client_socket.send(str(file_size).encode())  # Send the file size
             while True:
                 data = file.read(8388608)
                 if not data:
